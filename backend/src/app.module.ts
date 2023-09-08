a63a5566b337f6364aeca13aace4a0569bf98c2e
Alex <seremet.alexandru.work@gmail.com>
2023-09-08 13:29:18 +0300

    added redis

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,26 @@
-import { Module } from '@nestjs/common';
+import { redisStore } from 'cache-manager-redis-store';
+import { CacheStore, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        ttl: configService.get('CACHE_TTL'),
+        store: (await redisStore({
+          url: configService.get('REDIS_URL'),
+        })) as unknown as CacheStore,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
